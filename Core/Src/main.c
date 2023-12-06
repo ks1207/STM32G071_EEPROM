4aa2ad0c656f9a2b3fb9b5888b1453fb14524d30
ks1207 <ks1207@161.134.224.102>
2023-12-06 11:13:49 +0100

    update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -12,7 +12,7 @@
   * This software is licensed under terms that can be found in the LICENSE file
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
+  * update 6.12.2023
   ******************************************************************************
   */
 /* USER CODE END Header */
